b2296b2ff72edcbc88fac9976568ad2ad0be5493
Matheus Gusmão <matheus.lemos@ccc.ufcg.edu.br>
2018-11-20 14:20:24 -0200

    Atualizacões no controller de usuarios e na facade.

--- src/com/edoe/models/ControllerUsuario.java
+++ src/com/edoe/models/ControllerUsuario.java
@@ -2,54 +2,83 @@ package com.edoe.models;
 
 import java.util.HashMap;
 import java.util.Map;
+
 /**
+ * Classe que controla o usuario. Adiciona, pesquisa
+ * atualiza e remove usuarios.
  * 
+ * @author Matheus Gusmao
+ * @author Davidson Guedes
+ * @author Caroliny Silva
+ * @author Almir Crispiniano
  *
  */
 public class ControllerUsuario {
+	
 	/**
-	 * 
+	 * Atributo que representa os doadores.
 	 */
 	private Map<String, Doador> doadores;
 	
+	/**
+	 * Construtor do controlador de usuarios.
+	 */
 	public ControllerUsuario() {
 		this.doadores = new HashMap<>();
 	}
 	
 	/**
+	 * Metodo responsavel por adicionar um usuario doador.
 	 * 
 	 * @param nome
+	 * 		String que representa o nome do usuario doador.
+	 * 
 	 * @param id
+	 * 		String que representa o id do usuario doador.
+	 * 
 	 * @param email
+	 * 		String que representa o email do usuario doador.
+	 * 
 	 * @param celular
+	 * 		String que representa o celular do usuario doador.
+	 * 
 	 * @param classe
+	 * 		String que representa a classe do usuario doador.
 	 */
 	public void adicionaDoador(String nome, String id, String email, String celular, String classe) {
 		Doador doador = new Doador(nome, email, celular, id, classe);
 		this.doadores.put(id, doador);
-
 	}
+	
 	/**
+	 * Metodo responsavel por pesquisar um usuario atraves de sua identificacao.
 	 * 
 	 * @param id
-	 * @return
+	 * 		String que representa o id do usuario.
+	 * 
+	 * @return A representacao textual do usuario, por meio da pesquisa de seu id.
 	 */
 	public String pesquisaUsuarioPorId(String id) {
 		return this.doadores.get(id).toString();
 	}
+	
 	/**
-	 * 
+	 *  Metodo responsavel por atualizar um usuario no sistema.
+	 *  
 	 * @param id
-	 * @return
+	 * 		String que representa o id do usuario.
+	 * 
+	 * @return null.
 	 */
 	public String atualizaUsuario(String id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	/**
+	 * Metodo responsavel por remover um usuario do sistema.
 	 * 
 	 * @param id
-	 * @return
+	 * 		String que representa o id do usuario.
 	 */
 	public void removeUsuario(String id) {
 		this.doadores.remove(id);
--- src/com/edoe/models/Facade.java
+++ src/com/edoe/models/Facade.java
@@ -1,32 +1,94 @@
 package com.edoe.models;
+
 /**
+ * Classe que ira se comunicar com os controllers. 
+ * Facilitando assim a comunicacao com a interface.
  * 
- *
+ * @author Matheus Gusmao
+ * @author Davidson Guedes
+ * @author Caroliny Silva
+ * @author Almir Crispiniano
  */
 public class Facade {
+	
+	/**
+	 * Atributo referente ao controle de usuarios.
+	 */
 	private ControllerUsuario controle;
 
+	/**
+	 * Metodo responsavel pelo carregamento de dados.
+	 */
 	public void inicializa() {
 		controle = new ControllerUsuario();
 	}
 
+	/**
+	 * Metodo que adiciona um usuario doador no sistema.
+	 * 
+	 * @param nome
+	 * 		String que representa o nome do usuario doador.
+	 * 
+	 * @param id
+	 * 		String que representa o id do usuario doador.
+	 * 
+	 * @param email
+	 * 		String que representa o email do usuario doador.
+	 * 
+	 * @param celular
+	 * 		String que representa o celular do usuario doador.
+	 * 
+	 * @param classe
+	 * 		String que representa a classe do usuario doador.
+	 */
 	public void adicionaDoador(String nome, String id, String email, String celular, String classe) {
 		controle.adicionaDoador(nome, id, email, celular, classe);
 	}
 
+	/**
+	 * Metodo que pesquisa determinado usuario atraves de seu id.
+	 * 
+	 * @param id
+	 * 		String que representa o id do usuario.
+	 * 
+	 * @return A representacao textual do usuario procurado, se estiver
+	 * presente no sistema.
+	 */
 	public String pesquisaUsuarioPorId(String id) {
 		return controle.pesquisaUsuarioPorId(id);
 	}
 
+	/**
+	 * Metodo que pesquisa determinado usuario atraves do seu nome.
+	 * 
+	 * @param nome
+	 * 		String que representa o nome do usuario.
+	 * 
+	 * @return A representacao textual do usuario procurado, se estiver
+	 * presente no sistema.
+	 */
 	public String pesquisaUsuarioPorNome(String nome) {
 		return controle.pesquisaUsuarioPorId(nome);
 	}
 
+	/**
+	 * Metodo que atualiza um usuario atraves de seu id.
+	 * 
+	 * @param id
+	 * 		String que representa o id do usuario.
+	 * 
+	 * @return Null.
+	 */
 	public String atualizaUsuario(String id) {
 		return controle.atualizaUsuario(id);
-
 	}
 
+	/**
+	 * Metodo responsavel por remover um usuario do sistema.
+	 * 
+	 * @param id
+	 * 		String que representa o id do usuario.
+	 */
 	public void removeUsuario(String id) {
 		controle.removeUsuario(id);
 	}
