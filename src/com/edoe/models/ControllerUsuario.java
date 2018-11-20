e28989eb97768fcb9d10c2922d0814d71ecf60f0
CarolinySilva <caroliny.silva@ccc.ufcg.edu.br>
2018-11-20 11:29:40 -0300

    Facade

--- src/com/edoe/models/ControllerUsuario.java
+++ src/com/edoe/models/ControllerUsuario.java
@@ -2,5 +2,25 @@ package com.edoe.models;
 
 public class ControllerUsuario {
 
+	public void adicionaDoador(String nome, String id, String email, String celular, String classe) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String pesquisaUsuarioPorId(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String atualizaUsuario(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String removeUsuario(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	// classe de Davidson
 }
--- src/com/edoe/models/Facade.java
+++ src/com/edoe/models/Facade.java
@@ -1,5 +1,30 @@
 package com.edoe.models;
 
 public class Facade {
+	private ControllerUsuario controle;
 
+	public void inicializa() {
+		controle = new ControllerUsuario();
+	}
+
+	public void adicionaDoador(String nome, String id, String email, String celular, String classe) {
+		controle.adicionaDoador(nome, id, email, celular, classe);
+	}
+
+	public String pesquisaUsuarioPorId(String id) {
+		return controle.pesquisaUsuarioPorId(id);
+	}
+
+	public String pesquisaUsuarioPorNome(String nome) {
+		return controle.pesquisaUsuarioPorId(nome);
+	}
+
+	public String atualizaUsuario(String id) {
+		return controle.atualizaUsuario(id);
+
+	}
+
+	public String removeUsuario(String id) {
+		return controle.removeUsuario(id);
+	}
 }
